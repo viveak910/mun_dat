f5f74919a62307071e05d6136102a536ee163f4a
BTJ-V <bhanutejavaravenkata@Bhanus-MacBook-Pro.local>
2024-08-21 21:02:34 +0530

    two

--- api/index.js
+++ api/index.js
@@ -1,12 +1,41 @@
 const express = require("express");
-const app = express();
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 
-app.get("/advice", (req, res) => {
-  res.send("Probe the lobe the globe \n -Bhanu Teja");
+const app = express();
+const uri =
+  "mongodb+srv://bhanutejavaravenkatareddy:gmeyk55gg0Rwy7Nn@cluster0.erthl.mongodb.net/MUN?retryWrites=true&w=majority";
+
+app.use(express.json());
+app.use(cors());
+
+let db;
+let collection;
+
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to MongoDB Atlas");
+    db = client.db("MUN");
+    collection = db.collection("Registrations");
+  })
+  .catch((err) => console.error("Failed to connect to MongoDB Atlas", err));
+
+app.post("/registration", (req, res) => {
+  const registrationData = req.body;
+
+  collection
+    .insertOne(registrationData)
+    .then(() => {
+      res.status(201).send("Registration data saved successfully!");
+    })
+    .catch((error) => {
+      console.error("Error saving registration data", error);
+      res.status(500).send("Error saving registration data");
+    });
 });
 
 app.listen(3000, () => {
-  console.log("port 3000s");
+  console.log("Server is running on port 3000");
 });
 
 module.exports = app;
